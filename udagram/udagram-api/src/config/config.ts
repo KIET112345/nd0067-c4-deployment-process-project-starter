cc50e3b14d57d1cd6d52ccf20a7e8d82ba4df492
KIET112345 <vankietclbk@gmail.com>
2024-06-20 11:38:21 +0700

    deploy

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,15 +7,21 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
+  username: `${process.env.POSTGRES_USERNAME}`.split('\r')[0],
+  password: process.env.POSTGRES_PASSWORD.split('\r')[0],
+  database: process.env.POSTGRES_DB.split('\r')[0],
+  host: process.env.POSTGRES_HOST.split('\r')[0],
+  aws_region: process.env.AWS_REGION.split('\r')[0],
+  aws_profile: process.env.AWS_PROFILE.split('\r')[0],
+  aws_media_bucket: process.env.AWS_BUCKET.split('\r')[0],
   url: process.env.URL,
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET.split('\r')[0],
   },
 };
+console.log("config::", config);
+
+
+
+
+
