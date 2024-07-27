9b486f3ada5f5a9b2214814dd2b2c73681b7d4c3
KIET112345 <vankietclbk@gmail.com>
2024-07-27 10:49:26 +0700

    Implement flow CICD

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres12345
+export POSTGRES_HOST=database-project4.c7y8sg6am3qh.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::mybucket-641747160724
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export JWT_SECRET=mysecretstring12345
 export URL=http://localhost:8100
