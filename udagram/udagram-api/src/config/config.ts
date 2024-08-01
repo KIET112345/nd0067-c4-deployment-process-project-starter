eb8f3b2bac13306b02dbf8ac03934c55d9ef438b
KIET112345 <vankietclbk@gmail.com>
2024-08-01 20:52:49 +0700

    change config Be

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,15 +7,15 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  username: `${process.env.POSTGRES_USERNAME.split('\r')[0]}`,
+  password: `${process.env.POSTGRES_PASSWORD.split('\r')[0]}`,
+  database: `${process.env.POSTGRES_DB.split('\r')[0]}`,
+  host: `${process.env.POSTGRES_HOST.split('\r')[0]}`,
+  aws_region: `${process.env.AWS_REGION.split('\r')[0]}`,
+  aws_profile: `${process.env.AWS_PROFILE.split('\r')[0]}`,
+  aws_media_bucket: `${process.env.AWS_BUCKET.split('\r')[0]}`,
+  url: `${process.env.URL.split('\r')[0]}`,
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: `${process.env.JWT_SECRET.split('\r')[0]}`,
   },
 };
