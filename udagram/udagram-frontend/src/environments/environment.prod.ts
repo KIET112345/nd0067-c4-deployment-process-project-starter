aa4c27821d823c33036c1863be9b6ab6ff8df425
KIET112345 <vankietclbk@gmail.com>
2024-08-02 09:10:35 +0700

    revert code config

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'https://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'https://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
