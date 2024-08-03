a7284a0297d8632b0b6f6c41f342e8699e97f37a
KIET112345 <vankietclbk@gmail.com>
2024-08-03 12:09:09 +0700

    change env ui

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https:/project4-641747160724.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'https:/kiet-641747160724.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
