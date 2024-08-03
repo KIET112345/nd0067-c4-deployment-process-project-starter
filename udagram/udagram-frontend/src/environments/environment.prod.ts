4638cbd37a961714e3307c6c8739f2b604b9d635
KIET112345 <vankietclbk@gmail.com>
2024-08-03 10:59:26 +0700

    change s3 and e2c

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'https://project4-641747160724.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'https:/project4-641747160724.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
